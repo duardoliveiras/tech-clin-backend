1ed837e49edcb1d832cee14fc3eac23d532ed472
duardoliveiras <duardooliveiras@gmail.com>
2024-08-26 22:33:36 -0400

    fix: ajustando init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -2,7 +2,8 @@ create database tech_clin
 
 create extension if not exists "uuid-ossp"
 
-create table user (
+-- usando nome users pois user eh reservado pelo postgres
+create table users (
 	id uuid primary key default generate_uuid_v4(),
 	email varchar(255),
 	password varchar(255)
